6e6f97c033c6f62ad838ec33e5fcb4333760bf7e
evheny0 <evheny0@gmail.com>
2013-04-20 21:26:45 +0300

    works with unambiguous Sudoku

--- main.c
+++ main.c
@@ -14,6 +14,12 @@ typedef struct field_t {
 
 field_t **importField();
 void solvingSudoku(field_t **field);
+int checkingPossibleCells(field_t **field);
+void checkCellSquare(field_t **field, int x, int y, int *probableNum);
+void checkCellX(field_t **field, int x, int y, int *probableNum);
+void checkCellY(field_t **field, int x, int y, int *probableNum);
+void settingCells(field_t **field);
+void showField(field_t **field);
 
 int main(int argc, char *argv[])
 {
@@ -39,7 +45,9 @@ field_t **importField()
         for (j = 0; j < fieldSize; j++) {
             tempChar = getc(inputFile);
             field[i][j].cell = atoi(&tempChar);
-            field[i][j].isBasic = 1;
+            if (field[i][j].cell) {
+                field[i][j].isBasic = 1;
+            }
             printf("%d ", field[i][j].cell);            /**/
         }
         tempChar = getc(inputFile);
@@ -54,7 +62,130 @@ field_t **importField()
 
 void solvingSudoku(field_t **field)
 {
-    while (1) {
-        printf("SOLVING IN PROCESS.\n");
+    while (checkingPossibleCells(field)) {
+        settingCells(field);
+    }
+    settingCells(field);
+    showField(field);
+}
+
+int checkingPossibleCells(field_t **field)
+{
+    int i, j, k, probableNum[NUM_OF_VALUES], returnNumber = 0;
+    printf("\n");
+    for (i = 0; i < NUM_OF_VALUES; i++) {
+        for (j = 0; j < NUM_OF_VALUES; j++) {
+            if (!field[i][j].isBasic) {
+                field[i][j].probableNum = 0;
+                checkCellSquare(field, i, j, probableNum);
+                checkCellX(field, i, j, probableNum);
+                checkCellY(field, i, j, probableNum);
+                for (k = 0; k < NUM_OF_VALUES; k++) {
+                    if (probableNum[k]) {
+                        field[i][j].probable[field[i][j].probableNum] = k + 1;
+                        field[i][j].probableNum++;
+                    }
+                }
+                if (field[i][j].probableNum > 1) {
+                    returnNumber = 1;
+                }
+            }
+            printf("%d ", field[i][j].probableNum);
+        }
+        printf("\n");
+    }
+    return returnNumber;
+}
+
+void checkCellSquare(field_t **field, int x, int y, int *probableNum)
+{
+    int i, j, startX, startY, number;
+    startX = x - (x % 3);
+    startY = y - (y % 3);
+    for (number = 1; number <= NUM_OF_VALUES; number++) {
+        for (i = startX; i < (startX + 3); i++) {
+            for (j = startY; j < (startY + 3); j++) {
+                if (number == field[i][j].cell) {
+                    i = startX + 3;
+                    break;
+                }
+            }
+        }
+
+        if (j == (startY + 3) && i == (startX + 3)) {
+            probableNum[number - 1] = 1;
+        } else {
+            probableNum[number - 1] = 0;
+        }
+    }
+}
+
+void checkCellX(field_t **field, int x, int y, int *probableNum)
+{
+    int i, number;
+    for (number = 1; number <= NUM_OF_VALUES; number++) {
+        for (i = 0; i < NUM_OF_VALUES; i++) {
+            if (i == x) {
+                continue;
+            }
+            if (number == field[i][y].cell) {
+                break;
+            }
+        }
+
+        if (i == NUM_OF_VALUES && probableNum[number - 1]) {
+            probableNum[number - 1] = 1;
+        } else {
+            probableNum[number - 1] = 0;
+        }
+    }
+}
+
+void checkCellY(field_t **field, int x, int y, int *probableNum)
+{
+    int j, number;
+    for (number = 1; number <= NUM_OF_VALUES; number++) {
+        if (!probableNum[number - 1]) {
+            continue;
+        }
+        for (j = 0; j < NUM_OF_VALUES; j++) {
+            if (j == y) {
+                continue;
+            }
+            if (number == field[x][j].cell) {
+                break;
+            }
+        }
+
+        if (j == NUM_OF_VALUES && probableNum[number - 1]) {
+            probableNum[number - 1] = 1;
+        } else {
+            probableNum[number - 1] = 0;
+        }
+    }
+} 
+
+void settingCells(field_t **field)
+{
+    int i, j;
+    for (i = 0; i < NUM_OF_VALUES; i++) {
+        for (j = 0; j < NUM_OF_VALUES; j++) {
+            if (field[i][j].probableNum == 1) {
+                field[i][j].cell = field[i][j].probable[0];
+                field[i][j].probableNum--;
+            }
+        }
+    }
+}
+
+void showField(field_t **field)
+{
+    int i, j;
+    printf("\n");
+    for (i = 0; i < NUM_OF_VALUES; i++) {
+        for (j = 0; j < NUM_OF_VALUES; j++) {
+            printf("%d ", field[i][j].cell);
+        }
+        printf("\n");
     }
 }
